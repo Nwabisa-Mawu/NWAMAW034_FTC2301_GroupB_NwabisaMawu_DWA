b1dfdd646339a6844f3dcb096676959ff7ed3509
NwabisaMawu <nwabisa.mawu@gmail.com>
2023-06-08 03:02:25 +0200

    DWA8 final submission

--- DWA8/js/functions.js
+++ DWA8/js/functions.js
@@ -10,13 +10,59 @@ import {
   books, genres, authors, BOOKS_PER_PAGE,
 } from './data.js';
 
-import { data } from './scripts.js';
+/* --------------------------------------DOM ELEMENT FUNCTIONS----------------------------------- */
+/**
+ * Will fetch the DOM element
+ *
+ * @param {string} elementId
+ * @returns
+ */
+export const getHtml = (elementId) => {
+  const htmlElemenet = document.querySelector(elementId);
+  return htmlElemenet;
+};
+/**
+ * Will fetch a Node list of elements
+ *
+ * @param {string} nodeId
+ * @returns {NodeList}
+ */
+export const getHtmlArray = (nodeId) => {
+  const htmlElemenet = document.querySelectorAll(nodeId);
+  return htmlElemenet;
+};
 
-/* ---------------------------------------GLOBAL VARIABLES--------------------------------------- */
+/* -------------------------------------DOM ELEMENTS STORAGE OBJECT------------------------------ */
 
 /**
-   * This is the fragment used to create the book button lists html
-   */
+ * this object has the query selectors for all the DOM elements used in the javascript
+ */
+export const data = {
+  home: {
+    root: document.documentElement,
+    main: getHtml('[data-list-items]'),
+    showMoreBtn: getHtml('[data-list-button'),
+    logoText: getHtml('.header__logo'),
+    search: getHtml('[data-header-search]'),
+    themeBtn: getHtml('[data-header-settings]'),
+  },
+  summary: {
+    overlay: document.querySelector('[data-list-active]'),
+    close: document.querySelector('[data-list-close]'),
+  },
+};
+
+/* ---------------------------------------GLOBAL VARIABLES--------------------------------------- */
+export const {
+  home: {
+    root: ROOT,
+    main: HOME_PAGE,
+    showMoreBtn: SHOW_MORE_BTN,
+  },
+  summary: {
+    overlay: BOOK_SUMMARY,
+  },
+} = data;
 const FRAGMENT = document.createDocumentFragment();
 
 /* -----------------------------------------------DISPLAY---------------------------------------- */
@@ -27,12 +73,12 @@ const FRAGMENT = document.createDocumentFragment();
  * This creates the button element then loads the book information
  * before displaying it on the html page.
  *
- * @param {object} book
+ * @param {Array} book
  * @returns {Div} FRAGMENT
  */
 const createBookButtons = (book) => {
   /* create a button element for the books so each book is
-	  in its own card */
+    in its own card */
   const button = document.createElement('button');
   // create a class and call it preview
   button.classList.add('preview');
@@ -41,7 +87,7 @@ const createBookButtons = (book) => {
   // Set the button's inner HTML to the book's title and author.
   // eslint-disable-next-line operator-linebreak
   button.innerHTML = /* HTML markup for the book cards */
-		`
+    `
 	 <img class="preview__image" src="${book.image}" />
 	 <div class="preview__info">
 	   <h3 class="preview__title">${book.title}</h3>
@@ -60,10 +106,10 @@ const createBookButtons = (book) => {
  * loaded to the page
  */
 export const updateBooksLeft = () => {
-  const booksOnPage = data.home.bookCards;
+  const booksOnPage = getHtmlArray('.preview');
   const booksOnPageCount = booksOnPage.length;
   const booksLeft = books.length - booksOnPageCount;
-  return booksLeft;
+  return { booksLeft, booksOnPageCount };
 };
 
 /**
@@ -72,44 +118,38 @@ export const updateBooksLeft = () => {
  * @param {object} books
  */
 export const appendBooks = (books) => {
-  // eslint-disable-next-line no-plusplus
   books.slice(0, BOOKS_PER_PAGE).forEach((book) => {
     createBookButtons(book);
   });
   // Append the fragment to the data-list-items div.
-  data.home.main.appendChild(FRAGMENT);
-
-  data.home.showMoreBtn.innerHTML = `Show more <span class = "list__remaining">(${updateBooksLeft() - BOOKS_PER_PAGE})</span>`;
+  HOME_PAGE.appendChild(FRAGMENT);
+  SHOW_MORE_BTN.disabled = false;
+  SHOW_MORE_BTN.innerHTML = `Show more <span class = "list__remaining">(${updateBooksLeft().booksLeft})</span>`;
 };
 
 /**
-	 * This function will add more books to the page and update
-	 * the number in the show more button everytime it is clicked
-	 * until there are no more books left in the books object.
-   *
-   * @param {'click'} event
-	 */
+   * This function will add more books to the page and update
+   * the number in the show more button everytime it is clicked
+   * until there are no more books left in the books object.
+   *@param { Event } event
+   */
 export const showMoreAction = (event) => {
   event.preventDefault();
-  const booksOnPage = document.querySelectorAll('.preview');
-  const booksOnPageCount = booksOnPage.length;
-  const booksLeft = books.length - booksOnPageCount;
-
+  const { booksLeft, booksOnPageCount } = updateBooksLeft();
   if (booksLeft > 36) {
-    appendBooks(books.slice(booksOnPageCount, booksOnPageCount + 36));
-    data.home.showMoreBtn.innerHTML = `Show more <span class="list__remaining">(${booksLeft - BOOKS_PER_PAGE})</span>`;
-  } else {
-    books.slice(0, booksLeft).forEach((book) => {
+    books.slice(booksOnPageCount, booksOnPageCount + 36).forEach((book) => {
       createBookButtons(book);
     });
-
-    data.home.main.appendChild(FRAGMENT);
-    data.home.showMoreBtn.innerHTML = 'Show more <span class="list__remaining">(0)</span>';
-    data.home.showMoreBtn.disabled = true;
+  } else {
+    books.slice(booksOnPageCount).forEach((book) => {
+      createBookButtons(book);
+    });
+    SHOW_MORE_BTN.disabled = true;
   }
-
-  const bookList = document.querySelectorAll('.preview');
-  bookList.forEach((book) => {
+  HOME_PAGE.appendChild(FRAGMENT);
+  SHOW_MORE_BTN.innerHTML = `Show more <span class="list__remaining">(${updateBooksLeft().booksLeft})</span>`;
+  const booksOnPage = getHtmlArray('.preview');
+  booksOnPage.forEach((book) => {
     // eslint-disable-next-line no-use-before-define
     book.addEventListener('click', descriptionOverlay);
   });
@@ -123,38 +163,31 @@ export const showMoreAction = (event) => {
  */
 export const descriptionOverlay = (event) => {
   event.preventDefault();
-
-  const bookSummary = data.summary.overlay;
-  // get the book that is clicked by getting the closest element with the identifier to
-  // where the click happened.
   const book = event.target.closest('.preview');
-  // get a book id to use to fetch book information
   const bookId = book.getAttribute('data-preview');
-  // for loop to iterate over the book object lloking for matchind ids
-  for (let i = 0; i < books.length; i += 1) {
-    // check if the id in the books object matches that of the clicked book
-    if (books[i].id === bookId) {
-      // The book summary overlay html
-      bookSummary.innerHTML = /* html */
-				`<div class="overlay__preview">
-        <img class="overlay__blur" data-list-blur="" src="${books[i].image}">
-        <img class="overlay__image" data-list-image="" src="${books[i].image}">
+
+  books.forEach((book) => {
+    if (book.id === bookId) {
+      BOOK_SUMMARY.innerHTML = /* html */
+        `<div class="overlay__preview">
+        <img class="overlay__blur" data-list-blur="" src="${book.image}">
+        <img class="overlay__image" data-list-image="" src="${book.image}">
         </div>
         <div class="overlay__content">
-        <h3 class="overlay__title" data-list-title="">${books[i].title} (${new Date(books[i].published).getFullYear()})</h3>
-        <div class="overlay__data" data-list-subtitle="">${authors[books[i].author]}</div>
-        <p class="overlay__data overlay__data_secondary" data-list-description="">${books[i].description}</p>
+        <h3 class="overlay__title" data-list-title="">${book.title} (${new Date(book.published).getFullYear()})</h3>
+        <div class="overlay__data" data-list-subtitle="">${authors[book.author]}</div>
+        <p class="overlay__data overlay__data_secondary" data-list-description="">${book.description}</p>
         </div>
         <div class="overlay__row">
         <button class="overlay__button overlay__button_primary" data-list-close="">Close</button>
         </div>`;
     }
-  }
-  // show the book summary overlay when its done being created
-  bookSummary.showModal();
-  // when the close button is clicked, the overlay should be removed
-  document.querySelector('[data-list-close]').addEventListener('click', () => {
-    bookSummary.close();
+  });
+
+  BOOK_SUMMARY.showModal();
+
+  getHtml('[data-list-close]').addEventListener('click', () => {
+    BOOK_SUMMARY.close();
   });
 };
 
@@ -175,10 +208,10 @@ authorArray.unshift('All Authors');
 /**
  * This is the dialog box for the search overlay html
  */
-export const searchDialog = document.querySelector('[data-search-overlay]');
+export const searchDialog = getHtml('[data-search-overlay]');
 
 searchDialog.innerHTML = /* html */
-	`<div class="overlay__content">
+  `<div class="overlay__content">
       <form class="overlay__form" data-search-form="" id="search">
         <label class="overlay__field">
           <div class="overlay__label">Title</div>
@@ -210,7 +243,7 @@ searchDialog.innerHTML = /* html */
 export const handleSearchOverlay = (event) => {
   event.preventDefault();
   searchDialog.showModal();
-  data.search.title.focus();
+  getHtml('[data-search-title]').focus();
 };
 
 /**
@@ -224,9 +257,9 @@ export const handleSearchOverlay = (event) => {
 export const searchBooks = (event) => {
   event.preventDefault();
 
-  const searchText = document.querySelector('[data-search-title]').value.toLowerCase().trim();
-  const selectedGenre = document.querySelector('[data-search-genres]').value;
-  const selectedAuthor = document.querySelector('[data-search-authors]').value;
+  const searchText = getHtml('[data-search-title]').value.toLowerCase().trim();
+  const selectedGenre = getHtml('[data-search-genres]').value;
+  const selectedAuthor = getHtml('[data-search-authors]').value;
 
   let filteredBooks = books;
 
@@ -234,47 +267,35 @@ export const searchBooks = (event) => {
     const genreId = Object.keys(genres).find((key) => genres[key] === selectedGenre);
     filteredBooks = filteredBooks.filter((book) => book.genres.includes(genreId.toString()));
   }
-
   if (selectedAuthor !== 'All Authors') {
     const authorId = Object.keys(authors).find((key) => authors[key] === selectedAuthor);
     filteredBooks = filteredBooks.filter((book) => book.author.includes(authorId));
   }
-
   if (searchText !== '') {
     filteredBooks = filteredBooks.filter((book) => book.title.toLowerCase().includes(searchText));
   }
-
   const booksFound = filteredBooks.length > 0;
   // if there are no books found then should print message
   if ((!booksFound) || ((!searchText) && (selectedAuthor === 'All Authors') && (selectedGenre === 'All Genres'))) {
-    document.querySelector('[data-list-items]').innerHTML = '';
-    document.querySelector('[data-list-items]').innerHTML = `<div class = "list__message list__message_show" data-list-message = "">
+    HOME_PAGE.innerHTML = '';
+    HOME_PAGE.innerHTML = `<div class = "list__message list__message_show" data-list-message = "">
                                                               <p>No results found.
                                                               Your filters may be too narrow, try again</p>
                                                           </div>`;
 
-    // disable the show more button for the results page
-    data.home.showMoreBtn.disabled = true;
-
+    SHOW_MORE_BTN.disabled = true;
     return filteredBooks;
   }
-
   // Clear the book list on the homepage
-  data.home.main.innerHTML = '';
-
-  // Append the filtered books to the book list, used BOOKs_perpage to show only 36 books per page
-  filteredBooks.slice(0, BOOKS_PER_PAGE).forEach((book) => {
-    createBookButtons(book);
-  });
-  data.home.main.appendChild(FRAGMENT);
-
+  HOME_PAGE.innerHTML = '';
+  // Append the filtered books to the book list
+  appendBooks(filteredBooks);
+  HOME_PAGE.appendChild(FRAGMENT);
   // disable the show more button for the results page
-  data.home.showMoreBtn.disabled = true;
-
-  // Show the summary overlay on the search results
-  const searchResultList = data.home.main;
-  const searchResultBook = searchResultList.querySelectorAll('button');
-  for (const singleResult of searchResultBook) {
+  SHOW_MORE_BTN.disabled = true;
+  // the search results summary overlay
+  const searchResultList = HOME_PAGE.querySelectorAll('button');
+  for (const singleResult of searchResultList) {
     singleResult.addEventListener('click', descriptionOverlay);
   }
 };
@@ -284,10 +305,10 @@ export const searchBooks = (event) => {
 /**
  * This variable is the dialog box for the light/dark toggle overlay
  */
-export const lightToggleDialog = document.querySelector('[data-settings-overlay]');
+export const lightToggleDialog = getHtml('[data-settings-overlay]');
 
 lightToggleDialog.innerHTML = /* html */
-	`<div class="overlay__content">
+  `<div class="overlay__content">
                             <form class="overlay__form" data-settings-form="" id="settings">
                             <label class="overlay__field">
                               <div class="overlay__label">Theme</div>
@@ -322,7 +343,7 @@ export const changeTheme = (event) => {
     dark: '255, 255, 255',
     light: '10, 10, 20',
   };
-  const themeOption = document.querySelector('[data-settings-theme]').querySelectorAll('option');
+  const themeOption = getHtml('[data-settings-theme]').querySelectorAll('option');
   // first find the selected theme
   let selectedTheme = null;
   for (const singleOption of themeOption) {
@@ -330,15 +351,13 @@ export const changeTheme = (event) => {
       selectedTheme = singleOption.value;
     }
   }
-  // fetch the whole document to change its colors
-  const root = document.documentElement;
 
   if (selectedTheme.toLocaleLowerCase() !== 'night') {
-    root.style.setProperty('--color-dark', day.dark);
-    root.style.setProperty('--color-light', day.light);
+    ROOT.style.setProperty('--color-dark', day.dark);
+    ROOT.style.setProperty('--color-light', day.light);
   } else {
-    root.style.setProperty('--color-dark', night.dark);
-    root.style.setProperty('--color-light', night.light);
+    ROOT.style.setProperty('--color-dark', night.dark);
+    ROOT.style.setProperty('--color-light', night.light);
   }
   //  close the toggle overlay
   lightToggleDialog.close();
--- DWA8/js/scripts.js
+++ DWA8/js/scripts.js
@@ -12,62 +12,49 @@ import {
   handleSearchOverlay,
   searchDialog,
   lightToggleDialog,
+  getHtmlArray,
+  data,
+  ROOT,
+  SHOW_MORE_BTN,
+  HOME_PAGE,
 } from './functions.js';
 
-/* -------------------------------------DOM ELEMENTS STORAGE OBJECT------------------------------ */
+/* ---------------------------------------GLOBAL VARIABLES--------------------------------------- */
 
-/**
- * this object has the query selectors for all the DOM elements used in the javascript
- */
-export const data = {
+const {
   home: {
-    root: document.documentElement,
-    main: document.querySelector('[data-list-items]'),
-    showMoreBtn: document.querySelector('[data-list-button]'),
-    logoText: document.querySelector('.header__text'),
-    bookCards: document.querySelectorAll('preview'),
-    search: document.querySelector('[data-header-search]'),
-    themeBtn: document.querySelector('[data-header-settings]'),
+    logoText: LOGO,
+    search: HOME_SEARCH_BTN,
+    themeBtn: HOME_THEME_BTN,
   },
-  summary: {
-    overlay: document.querySelector('[data-list-active]'),
-    close: document.querySelector('[data-list-close]'),
-  },
-  search: {
-    overlay: document.querySelector('[data-search-overlay]'),
-    title: document.querySelector('[data-search-title]'),
-    searchBtn: document.querySelector('[data-search-overlay]').querySelectorAll('button')[1],
-    searchCancelBtn: document.querySelector('[data-search-overlay]').querySelectorAll('button')[0],
-  },
-  theme: {
-    overlay: document.querySelector('[data-settings-overlay]'),
-    toggleSaveBtn: document.querySelector('[data-settings-overlay]').querySelectorAll('button')[1],
-    toggleCancelBtn: document.querySelector('[data-settings-overlay]').querySelectorAll('button')[0],
-  },
-};
+} = data;
+const SEARCH_BTN = searchDialog.querySelectorAll('button')[1];
+const SEARCH_CANCEL_BTN = searchDialog.querySelectorAll('button')[0];
+const SAVE_CHANGES_BTN = lightToggleDialog.querySelectorAll('button')[1];
+const CANCEL_CHANGE_BTN = lightToggleDialog.querySelectorAll('button')[0];
 
 /* ---------------------------------------HOME PAGE DISPLAY-------------------------------------- */
 
 /* calling the function to load the page with book list using an event
 listener for when the page first loads  */
-data.home.root.addEventListener('load', appendBooks(books));
+ROOT.addEventListener('load', appendBooks(books));
 
 /* use event listener to make button load more books with the
 showMoreAction function */
-data.home.showMoreBtn.addEventListener('click', showMoreAction);
+SHOW_MORE_BTN.addEventListener('click', showMoreAction);
 
 /* this event listener return to home button when you click on the book connect
 text and logo */
-data.home.logoText.addEventListener('click', (event) => {
+LOGO.addEventListener('click', (event) => {
   event.preventDefault();
   // Clear the book list on the homepage
-  document.querySelector('[data-list-items]').innerHTML = '';
+  HOME_PAGE.innerHTML = '';
   // call this function to load the page again
   appendBooks(books);
 });
 
 /* make the summary overlay show when a book is clicked */
-const bookList = document.querySelectorAll('.preview');
+const bookList = getHtmlArray('.preview');
 // eslint-disable-next-line no-restricted-syntax
 for (const singleBook of bookList) {
   singleBook.addEventListener('click', descriptionOverlay);
@@ -75,38 +62,32 @@ for (const singleBook of bookList) {
 
 /* ----------------------------------------------SEARCH------------------------------------------ */
 
+HOME_SEARCH_BTN.addEventListener('click', handleSearchOverlay);
+
 // this is to carry out the book search when the search button is clicked
-data.search.searchBtn.addEventListener('click', searchBooks);
-
-// this is to close the overlay when the search is done
-data.search.searchBtn.addEventListener('click', (event) => {
+SEARCH_BTN.addEventListener('click', (event) => {
+  event.preventDefault();
+  searchBooks(event);
+  searchDialog.close();
+});
+SEARCH_CANCEL_BTN.addEventListener('click', (event) => {
   event.preventDefault();
   searchDialog.close();
 });
-
-// this is to close the search overlay when the cancel button is clicked
-data.search.searchCancelBtn.addEventListener('click', (event) => {
-  event.preventDefault();
-  searchDialog.close();
-});
-
-/* event listener for the search button to bring out the overlay */
-const homeSearchBtn = data.home.search;
-homeSearchBtn.addEventListener('click', handleSearchOverlay);
 
 /* ----------------------------------------LIGHT/DARK TOGGLE--------------------------------------*/
 
 // This is the event listener that shows the light/dark toggle overlay
-data.home.themeBtn.addEventListener('click', (event) => {
+HOME_THEME_BTN.addEventListener('click', (event) => {
   event.preventDefault();
   lightToggleDialog.showModal();
 });
 
 /* event listener for the save button in the light/dark toggle dialog */
-data.theme.toggleSaveBtn.addEventListener('click', changeTheme);
+SAVE_CHANGES_BTN.addEventListener('click', changeTheme);
 
 /* event listener for cancel button to remove overlay */
-data.theme.toggleCancelBtn.addEventListener('click', (event) => {
+CANCEL_CHANGE_BTN.addEventListener('click', (event) => {
   event.preventDefault();
   lightToggleDialog.close();
 });
